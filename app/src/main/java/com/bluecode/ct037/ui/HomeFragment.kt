fc72fd603945ecd52481863f5bb199d2e0c73e72
xCall <carlosfaita15@gmail.com>
2025-04-26 14:20:21 -0300

    finalizado versao 1

--- app/src/main/java/com/bluecode/ct037/ui/HomeFragment.kt
+++ app/src/main/java/com/bluecode/ct037/ui/HomeFragment.kt
@@ -1,15 +1,23 @@
 package com.bluecode.ct037.ui
 
 import android.content.Context
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.bluecode.ct037.R
 import com.bluecode.ct037.databinding.FragmentHomeBinding
+import java.util.Calendar
+import java.util.TimeZone
 import java.util.concurrent.TimeUnit
 
 enum class Button(val value: Int) {
@@ -25,9 +33,16 @@ class HomeFragment : Fragment() {
     private var setUptime: Int = 0
     private var setRestTime: Int = 0
 
+    private var timeRest: Boolean = false
+
     private var countDownTimer: CountDownTimer? = null
     private var isTimerRunning = false
     private var timeLeftInMillis: Long = 0
+
+    private var handler = Handler(Looper.getMainLooper())
+    private lateinit var updatedTimeRunnable: Runnable
+    private lateinit var tvUtcClockText: TextView
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +57,24 @@ class HomeFragment : Fragment() {
         loadDate()
         defaultCountdown()
 
+        tvUtcClockText = view.findViewById(R.id.tvUtcClockText)
         with(binding) {
+
+            updatedTimeRunnable = object : Runnable {
+                override fun run() {
+                    val currentTime = Calendar.getInstance(TimeZone.getTimeZone("America/Sao_Paulo"))
+                    val hour = currentTime.get(Calendar.HOUR_OF_DAY)
+                    val minute = currentTime.get(Calendar.MINUTE)
+                    val second = currentTime.get(Calendar.SECOND)
+
+                    val timeFormatted = String.format("%02d:%02d:%02d", hour,minute,second)
+                    tvUtcClockText.text = timeFormatted
+                    handler.postDelayed(this,1000)
+                }
+
+            }
+            handler.post(updatedTimeRunnable)
+
             disabledButtons(Button.BtnStart.value,true)
             disabledButtons(Button.BtnPause.value,false)
             btnOpenSettings.setOnClickListener{
@@ -50,11 +82,29 @@ class HomeFragment : Fragment() {
             }
 
             llBtnHelpers.getChildAt(Button.BtnStart.value).setOnClickListener{
-                if(!isTimerRunning){
+                if(!isTimerRunning && !timeRest){
+//                    if(timeLeftInMillis == 0L) {
+//                        binding.tvClockText.setTextColor(ContextCompat.getColor(requireContext(), R.color.yellow_400))
+//                        binding.tvClockText.text = "Lutar"
+//                        Handler(Looper.getMainLooper()).postDelayed({
+//
+//                            binding.tvClockText.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
+//                            startCountdown()
+//                        },2000)
+//
+//                    } else {
                     startCountdown()
+//
+//                    }
+                    disabledButtons(Button.BtnStart.value,false)
+                    disabledButtons(Button.BtnPause.value,true)
+
+                } else {
+                    restCountdown()
                     disabledButtons(Button.BtnStart.value,false)
                     disabledButtons(Button.BtnPause.value,true)
                 }
+
             }
 
             llBtnHelpers.getChildAt(Button.BtnPause.value).setOnClickListener{
@@ -105,6 +155,7 @@ class HomeFragment : Fragment() {
     }
 
     private fun resetCountdown() {
+        timeLeftInMillis = 0L
         stopCountdown()
         defaultCountdown()
     }
@@ -115,82 +166,64 @@ class HomeFragment : Fragment() {
         binding.tvClockText.text = String.format("%02d:%02d", minutes, seconds)
     }
 
-//    private fun startCountdown() {
-//
-//        val initialTimeInMinutes = if (setUptime == 0) 5 else setUptime!!
-//        val startTime = if (timeLeftInMillis > 0) timeLeftInMillis else initialTimeInMinutes * 60 * 1000L
-//
-//        countDownTimer = object : CountDownTimer(startTime,1000) {
-//            override fun onTick(millisUntilFinished:Long) {
-//                timeLeftInMillis = millisUntilFinished // Atualiza o tempo restante
-//                val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
-//                val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60
-//                binding.tvClockText.text = String.format("%02d:%02d", minutes, seconds)
-//            }
-//
-//            override fun onFinish() {
-//                binding.tvClockText.text = "04:00"
-//                isTimerRunning = true
-//
-//                restCountdown()
-//            }
-//        }.start()
-//
-//        isTimerRunning = true
-//    }
-//
-//    private fun restCountdown() {
-//
-//        val initialTimeInMinutes = if (setRestTime == 0) 5 else setRestTime!!
-//        val startTime = if (timeLeftInMillis > 0) timeLeftInMillis else initialTimeInMinutes * 60 * 1000L
-//
-//        countDownTimer = object : CountDownTimer(startTime,1000) {
-//            override fun onTick(millisUntilFinished:Long) {
-//                timeLeftInMillis = millisUntilFinished // Atualiza o tempo restante
-//                val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
-//                val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60
-//                binding.tvClockText.text = String.format("%02d:%02d", minutes, seconds)
-//            }
-//
-//            override fun onFinish() {
-//                binding.tvClockText.text = "04:00"
-//                isTimerRunning = true
-//
-//                startCountdown()
-//            }
-//        }.start()
-//
-//        isTimerRunning = true
-//    }
-
     private fun startCountdown() {
+        timeRest = false
         val initialTimeInMinutes = if (setUptime == 0) 5 else setUptime
-        timeLeftInMillis = initialTimeInMinutes * 60 * 1000L
+        if (timeLeftInMillis == 0L) {
+            message("Lutar")
 
-        countDownTimer?.cancel()
-        countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                timeLeftInMillis = millisUntilFinished
-                val minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished)
-                val seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60
-                binding.tvClockText.text = String.format("%02d:%02d", minutes, seconds)
-            }
+            timeLeftInMillis = initialTimeInMinutes * 60 * 1000L
 
-            override fun onFinish() {
-                isTimerRunning = false
-                timeLeftInMillis = 0L
-                restCountdown() // Vai para o descanso
-            }
-        }.start()
+            Handler(Looper.getMainLooper()).postDelayed({
+                setBackgroundColor()
+                countDownTimer(timeLeftInMillis, ::restCountdown)
+            }, 2000)
 
-        isTimerRunning = true
+        } else {
+            countDownTimer(timeLeftInMillis, ::restCountdown)
+        }
+
+    }
+
+    private fun setBackgroundColor() {
+        binding.root.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+    }
+
+    private fun message(message: String){
+        binding.tvClockText.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+        binding.tvClockText.text = message
+        val gradient = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM,
+            intArrayOf(0xFFF7C400.toInt(), 0xFFFFD600.toInt())
+        )
+
+        binding.root.background = gradient
     }
 
     private fun restCountdown() {
-        val initialRestTimeInMinutes = if (setRestTime == 0) 1 else setRestTime
-        timeLeftInMillis = initialRestTimeInMinutes * 60 * 1000L
 
+
+        val initialRestTimeInMinutes = if (setRestTime == 0) 1 else setRestTime
+        if (timeLeftInMillis == 0L) {
+            timeRest = true
+            message("Descansar")
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                setBackgroundColor()
+                countDownTimer(timeLeftInMillis, ::startCountdown)
+            }, 2000)
+
+            timeLeftInMillis = initialRestTimeInMinutes * 60 * 1000L
+        } else {
+            countDownTimer(timeLeftInMillis, ::startCountdown)
+        }
+
+    }
+
+    fun countDownTimer(initialRestTimeInMinutes: Long,countDown: () -> Unit) {
         countDownTimer?.cancel()
+
+        binding.tvClockText.setTextColor(ContextCompat.getColor(requireContext(), R.color.black))
         countDownTimer = object : CountDownTimer(timeLeftInMillis, 1000) {
             override fun onTick(millisUntilFinished: Long) {
                 timeLeftInMillis = millisUntilFinished
@@ -202,17 +235,20 @@ class HomeFragment : Fragment() {
             override fun onFinish() {
                 isTimerRunning = false
                 timeLeftInMillis = 0L
-                startCountdown() // Volta para o trabalho
+                Handler(Looper.getMainLooper()).postDelayed({
+
+                }, 2000) // 300
+                countDown() // Volta para o trabalho
             }
         }.start()
 
         isTimerRunning = true
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         countDownTimer?.cancel()
+        handler.removeCallbacks(updatedTimeRunnable)
 
         _binding = null
     }
